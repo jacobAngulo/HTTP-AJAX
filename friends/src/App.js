3dd807d086d06504b8652b5f981a57de55e926ae
Jacob Angulo <jacobangulo@hotmail.com>
2019-02-28 14:03:52 -0800

    added routing and navliinks to app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import FriendsList from './components/FriendsList';
-import AddFriendForm from './components/AddFriendForm'
+import AddFriendForm from './components/AddFriendForm';
+import { Route, NavLink } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
@@ -21,10 +22,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <FriendsList
-        friends = {this.state.friends}
-        />
-        <AddFriendForm />
+        <nav>
+          <NavLink to='/'>friends list</NavLink>
+          <NavLink to='/add-friend-form'>add friend</NavLink>
+        </nav>
+        <Route exact path='/' render={() => <FriendsList friends = {this.state.friends}/>}/>
+        <Route path='/add-friend-form' render={() => <AddFriendForm/>}/>
       </div>
     );
   }
